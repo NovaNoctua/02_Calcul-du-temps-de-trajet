234fa6081da21ad54872572053eca58da947aab7
Nova <m01@naudet.ch>
2026-09-23 13:56:26 +0200

    feat: user entry for L1;

--- main.cpp
+++ main.cpp
@@ -9,16 +9,30 @@ Remarque(s) :
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int main() {
     // Initialisation of the constants
     const double dx = 3.;
-    const double dy = 3.;
+    const double dy = 10.;
     const double s1 = 5.;
     const double s2 = 2.;
-    const double L1 = 6.;
+
+
+    // User Entry to get L1
+    double L1;
+    cout << "Entrez la distance a parcourir sur la route : ";
+
+    // User Entry and validation to be certain that it is possible.
+    // https://stackoverflow.com/questions/3273993/how-do-i-validate-user-input-as-a-double-in-c
+    while (!(cin >> L1) || (L1 < 0 || L1 > dy)) {
+        cout << "Veuillez entrer un nombre entre 0 et " << dy << " : ";
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+    cout << "Distance sur la route : " << L1 << endl;
 
     // Distance in the dirt
     const double distance_dirt_y = dy - L1;
@@ -31,4 +45,6 @@ int main() {
     const double totalTime = t1 + t2;
 
     cout << "Temps total pris par le robot : " << totalTime << " secondes" << endl;
+
+    return EXIT_SUCCESS;
 }
